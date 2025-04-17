0bf9b4520d5a4dc9c5aa06b612cf2a4961990acb
Hannah Kallias <hannahkallias@Hannahs-MacBook-Air.local>
2025-04-17 10:12:06 -0600

    final

--- finalproject.js
+++ finalproject.js
@@ -1,20 +1,59 @@
-originalValue = 1000;
 class Transaction {
     value = 0;
     date;
     
     constructor(valueAmount, year, month, day) {
-         this.value = valueAmount;
-         this.date = new Date(year, month, day);
+        this.value = valueAmount;
+        this.date = new Date(year, month, day);
     }
 
     toHTML() {
         this.divider = document.createElement("p");
-        this.divider.innerHTML = this.value + " " + this.date;
-        console.log(this.divider);
+        this.divider.innerHTML = `$${this.value} at ${this.date}`;
         return this.divider;
-    }
+    } 
 }
 
-johnTransaction = new Transaction(34, 2001, 6, 14);
-johnTransaction.toHTML();
+
+let transactionList = [];
+
+// Unused
+function saveItems() {
+    localStorage.setItem('budgetItems', JSON.stringify(transactionList));
+};
+
+// also unused
+function loadItems() {
+    const data = localStorage.getItem('budgetItems');
+    return data ? JSON.parse(data) : [];
+};
+
+function processTransactions() {
+    let sum = 0;
+    document.getElementById("transactionList").innerHTML = "";
+    for (const transaction of transactionList) {
+        sum += transaction.value;
+        document.getElementById("transactionList").appendChild(transaction.toHTML())
+    }
+    document.getElementById("balance").innerHTML = sum;
+}
+
+document.getElementById("addBtn").addEventListener("click", () => {
+    const amount = parseFloat(document.getElementById("amount").value);
+    const year = parseInt(document.getElementById("year").value);
+    const month = parseInt(document.getElementById("month").value);
+    const day = parseInt(document.getElementById("day").value);
+
+    if(!isNaN(amount) && year && month && day){
+        const tx = new Transaction(amount, year, month, day);
+        transactionList.push(tx);
+        processTransactions();
+
+        document.getElementById("amount").value = "";
+        document.getElementById("year").value = "";
+        document.getElementById("month").value = "";
+        document.getElementById("day").value = "";
+    } else {
+        alert("please enter the information correctly!");
+    }
+});
